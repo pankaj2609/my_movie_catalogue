8451b2daa294dde997e17378c792344828912247
pankaj2609 <92098113+pankaj2609@users.noreply.github.com>
2023-08-02 01:28:04 +0530

    Update app_info.js

--- app_info.js
+++ app_info.js
@@ -6,18 +6,10 @@ var url = "https://www.omdbapi.com/?t="+ movie_clicked +"&apikey=2761aa58";
 
 var p = fetch(url)
 
-// var movie_data; // declaring variable here
-
 p.then((data)=>{
     return (data.json());
 }).then((value2)=>{
-    // console.log(value2)
-    // movie_data = value2;// assigning values here
 
-    // console.log(movie_data.Genre,
-    //     movie_data.Language,
-    //     movie_data.Released,
-    //     movie_data.Runtime)
     console.log(value2)
         var text_data = 
     `<ul>
@@ -59,15 +51,3 @@ p.then((data)=>{
     
 
 })
-// console.log(movie_data) // using it here, it does not work, why????
-
-// var text_details = document.querySelector(".text-details");
-// var text_data = 
-// `<ul>
-//     <li>${movie_data.Genre}</li>
-//     <li>${movie_data.Language}</li>
-//     <li>${movie_data.Released}</li>
-//     <li>${movie_data.Runtime}</li>
-// </ul>`
-
-// text_details.insertAdjacentHTML("beforeend",text_data);
